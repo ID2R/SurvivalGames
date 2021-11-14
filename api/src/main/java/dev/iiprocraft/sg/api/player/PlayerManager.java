c883b1bc5c75f700fbb881802b78ac0980607c18
AbderrahimLach$ <serversreport15@gmail.com>
2021-11-14 21:57:07 +0100

    Starting progress on the player management

--- api/src/main/java/dev/iiprocraft/sg/api/player/PlayerManager.java
+++ api/src/main/java/dev/iiprocraft/sg/api/player/PlayerManager.java
@@ -1,4 +1,13 @@
 package dev.iiprocraft.sg.api.player;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class PlayerManager {
+
+    private final Map<UUID, SGPlayer> players = new HashMap<>();
+
+
+
 }
