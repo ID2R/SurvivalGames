21f019de1bc73f790b8d89ee6a6062df21869cbe
AbderrahimLach$ <serversreport15@gmail.com>
2021-11-15 16:22:39 +0100

    Player statistics

---
+++ api/src/main/java/dev/iiprocraft/sg/api/player/SGPlayerStatistics.java
@@ -0,0 +1,17 @@
+package dev.iiprocraft.sg.api.player;
+
+/**
+ * @author DirectPlan
+ */
+public interface SGPlayerStatistics {
+
+    int getWins();
+
+    int getLoses();
+
+    int getKills();
+
+    int getCoins();
+
+    // Probably prestige too
+}
