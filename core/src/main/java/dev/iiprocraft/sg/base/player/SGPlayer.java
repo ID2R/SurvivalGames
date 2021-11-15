f89890da4caf881a37dff8b6b13a6b2b14797a20
AbderrahimLach$ <serversreport15@gmail.com>
2021-11-15 18:26:38 +0100

    Changes

--- core/src/main/java/dev/iiprocraft/sg/base/player/SGPlayer.java
+++ core/src/main/java/dev/iiprocraft/sg/base/player/SGPlayer.java
@@ -1,22 +1,17 @@
 package dev.iiprocraft.sg.base.player;
 
 import lombok.Data;
-import lombok.Getter;
 import lombok.Setter;
+
 import java.util.UUID;
 
 @Data
 public class SGPlayer implements dev.iiprocraft.sg.api.player.SGPlayer {
 
-    @Getter private final UUID id;
+    private final UUID id;
     @Setter private SGPlayerStatistics playerStatistics;
 
     public SGPlayer(UUID id) {
         this.id = id;
-    }
-
-    @Override
-    public dev.iiprocraft.sg.api.player.SGPlayerStatistics getPlayerStatistics() {
-        return null;
     }
 }
