01ab4cafb30890808e730df14d8d8e00ffd75b58
AbderrahimLach$ <serversreport15@gmail.com>
2021-11-15 17:55:21 +0100

    Removed un-necessary methods

--- core/src/main/java/dev/iiprocraft/sg/base/player/SGPlayerStatistics.java
+++ core/src/main/java/dev/iiprocraft/sg/base/player/SGPlayerStatistics.java
@@ -2,8 +2,6 @@ package dev.iiprocraft.sg.base.player;
 
 import lombok.Data;
 
-import java.util.Objects;
-
 /**
  * @author DirectPlan
  */
@@ -11,17 +9,4 @@ import java.util.Objects;
 public class SGPlayerStatistics implements dev.iiprocraft.sg.api.player.SGPlayerStatistics {
 
     private int wins, loses, kills, coins;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SGPlayerStatistics that = (SGPlayerStatistics) o;
-        return wins == that.wins && loses == that.loses && kills == that.kills && coins == that.coins;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(wins, loses, kills, coins);
-    }
 }
