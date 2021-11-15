8aab0928dfc6e84812ae058f8603b9e5c41d9d6c
AbderrahimLach$ <serversreport15@gmail.com>
2021-11-15 16:35:53 +0100

    API registerer

---
+++ api/src/main/java/dev/iiprocraft/sg/api/SurvivalGamesProvider.java
@@ -0,0 +1,15 @@
+package dev.iiprocraft.sg.api;
+
+import lombok.Getter;
+
+/**
+ * @author DirectPlan
+ */
+public class SurvivalGamesProvider {
+
+    @Getter private static SurvivalGames api;
+
+    public static void registerApi(SurvivalGames api) {
+        SurvivalGamesProvider.api = api;
+    }
+}
