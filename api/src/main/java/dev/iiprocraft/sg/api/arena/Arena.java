69f8f5850a66ed17ab2ce7cbddb89e9c1c880175
72102072+mqzn@users.noreply.github.com <mezoahmed2507@gmail.com>
2021-11-15 20:10:30 +0200

    Implemented ItemRandomizer

--- api/src/main/java/dev/iiprocraft/sg/api/arena/Arena.java
+++ api/src/main/java/dev/iiprocraft/sg/api/arena/Arena.java
@@ -1,6 +1,15 @@
 package dev.iiprocraft.sg.api.arena;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author DirectPlan
@@ -20,4 +29,69 @@ public interface Arena {
     String getName();
 
     void setState(GameState state);
+
+    class ItemsRandomizer {
+
+        /**
+         * @author Mqzn
+         * @date 15/11/2021
+         */
+
+
+        private final RandomList<ItemStack> items;
+        private final HashMap<Location, Inventory> slots;
+        private final static int CHEST_SIZE = 26;
+
+        public ItemsRandomizer(Set<Location> chests, RandomList<ItemStack> items) {
+            this.items = items;
+
+            slots = new HashMap<>();
+            for(Location loc : chests) {
+                slots.putIfAbsent(loc, Bukkit.createInventory(null, 27, "A Chest"));
+            }
+        }
+
+
+        public RandomList<ItemStack> getRemainingItems() {
+            return items;
+        }
+
+        public Set<Location> getChests() {
+            return slots.keySet();
+        }
+
+
+        public void process() {
+
+            for(Location location : slots.keySet()) {
+
+                Inventory newInv = slots.get(location);
+
+
+                for (int i = 0; i < CHEST_SIZE+1 ; i++) {
+                    int slot = this.getNextRandomSlot(location);
+                    newInv.setItem(slot, items.next());
+
+                }
+
+                slots.compute(location, (loc, inv) -> newInv);
+            }
+
+        }
+
+        private int getNextRandomSlot(Location chest) {
+            Inventory slots = this.slots.get(chest);
+
+            int result = ThreadLocalRandom.current().nextInt(0, CHEST_SIZE);
+            while (slots.getItem(result) != null && slots.getItem(result).getType() != Material.AIR) {
+                result = ThreadLocalRandom.current().nextInt(0, CHEST_SIZE);
+            }
+
+            this.slots.put(chest, slots);
+
+            return result;
+        }
+
+    }
+
 }
