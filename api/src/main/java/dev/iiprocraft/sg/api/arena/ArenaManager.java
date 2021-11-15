d6020f1586a38abafc90fb1e8933c45c7e3121cc
AbderrahimLach$ <serversreport15@gmail.com>
2021-11-15 16:09:28 +0100

    the arena manager interface for the api

--- api/src/main/java/dev/iiprocraft/sg/api/arena/ArenaManager.java
+++ api/src/main/java/dev/iiprocraft/sg/api/arena/ArenaManager.java
@@ -1,50 +1,13 @@
- /*
- * MIT License
- *
- * Copyright (c) 2021 iiProCraft & contributors
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package dev.iiprocraft.sg.api.arena;
 
- import java.util.Collection;
- import java.util.Map;
- import java.util.concurrent.ConcurrentHashMap;
+import java.util.Collection;
 
- public class ArenaManager {
+/**
+ * @author DirectPlan
+ */
+public interface ArenaManager {
 
-    private final Map<String, SGArena> arenas;
+    Collection<? extends Arena> getArenas();
 
-    public ArenaManager() {
-        this.arenas = new ConcurrentHashMap<>();
-
-        //TODO load arenas from whatever storage selected for this project
-    }
-
-    public Collection<? extends SGArena> getArenas() {
-        return arenas.values();
-    }
-
-    public SGArena getArena(String arenaName) {
-        return arenas.get(arenaName);
-    }
-
-
+    Arena getArena(String arenaName);
 }
